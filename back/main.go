3c51cc9d94f9d36b3bc3e72a55952b497abc6b63
rjoaozinho1 <joaopedrocostamachado2003@gmail.com>
2024-12-06 20:57:08 -0300

    fixing the main names and testing

--- back/main.go
+++ back/main.go
@@ -3,13 +3,13 @@ package main
 import "log"
 
 func main() {
-	storage, err := newStorageConnection()
+	storage, err := NewStorageConnection()
 	if err != nil {
 		log.Fatal(err)
 	}
 	if err := storage.Init(); err != nil {
 		log.Fatal(err)
 	}
-	server := newApiServer(":2026", storage)
+	server := NewApiServer(":2026", storage)
 	server.Run()
 }
